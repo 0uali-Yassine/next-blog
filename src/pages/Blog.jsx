6f30f0a7730baa9021f9802003bda3333d936b98
meriemmouhim <mouhimmeriem9@gmail.com>
2025-02-26 23:52:46 +0000

    meriem

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,25 @@
+
+
+const Blog = () => {
+  return (
+    <div className='hero-section'>
+    <div className="hero-container">
+      <img src="http://localhost:5173/COVER.jpg" alt="Hero Image" />
+      <div className="overlay">
+        <h6>Animales | 26-02-2025</h6>
+        <h1>The Leading Voice in Unlocking Potential</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem quaerat ex illo sit voluptate blanditiis iure quidem a quia minus, numquam itaque inventore reiciendis dolores libero quo dignissimos tempore sed!</p>
+        <button>read more</button>
+      </div>
+    </div> 
+    <h1 className="ph">The Number Of Local, Independent Vets Is Shrinking Fast — And It Shows </h1>
+    <h4 className="pc">
+        Market consolidation in pet health has been going on for some time now. A deeper look at what’s behind this figure begins to uncover the unsavoury truth- some companies gobbling up veterinary care practices have, at first glance, mostly very little to do with the industry they’re buying up. Some are confectionary giants, some are private equity heavy hitters with diverse portfolios.
+        For example, Mars is one of the companies that has been an active gobbler for a while. Yes, Mars. Whose main original USP is providing us with chocolate bars and other confectionary. They began by buying the pet food company behind “Chappie” in 1935, then expanded into cat food that same decade. Then in 1965, they opened a research center in the UK to research feline nutrition needs. They continued acquiring pet-related businesses for the next few decades — for example, Royal Canin in 2002.
+        Their first foray into end-to-end pet health was in 1994 in the US, where they invested in Banfield Pet Hospital. These guys are the biggest veterinary practice in North America.
+    </h4>
+    </div>
+  )
+}
+
+export default Blog
