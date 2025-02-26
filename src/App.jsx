e37769cc25921dd64c6f3ac20aec153e0137e99a
0uali-Yassine <yassinzerouali0@gmail.com>
2025-02-26 22:08:59 +0000

    basic code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import Footer from './component/Footer'
+import PostListing from './component/PostListing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl text-red-700 font-bold underline">
-         Hello world!
-      </h1>
+      <PostListing />
+      <Footer />
     </>
   )
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-brand">
+            <div className="brand-logo">
+              <h1 className="brand-title">NextBlog</h1>
+            </div>
+            <p>
+              Our vision is to make all people <br />
+              the best place to live for them.
+            </p>
+          </div>
+          <div className="footer-links">
+            <ul>
+              <li className="link-title">About</li>
+              <li><p>About Us</p></li>
+              <li><p>Features</p></li>
+              <li><p>News & Blog</p></li>
+            </ul>
+            <ul>
+              <li className="link-title">Company</li>
+              <li><p>How We Work?</p></li>
+              <li><p>Capital</p></li>
+              <li><p>Security</p></li>
+            </ul>
+            <ul>
+              <li className="link-title">Support</li>
+              <li><p>FAQs</p></li>
+              <li><p>Support Center</p></li>
+              <li><p>Contact Us</p></li>
+            </ul>
+            <ul>
+              <li className="link-title">Movement</li>
+              <li><p>What is Homyz?</p></li>
+              <li><p>Support Us</p></li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&#169; NextBlog. All rights reserved</p>
+          <ul>
+            <li><p>Terms & Agreements</p></li>
+            <li><p>Privacy Policy</p></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
