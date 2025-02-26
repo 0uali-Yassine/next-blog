42a9fa16456b249eb86390c7b71695a64dcacf25
Fadwabaali <Fadwaaitbaali2@gmail.com>
2025-02-26 22:09:06 +0000

    Navbar and Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './component/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className="text-3xl text-red-700 font-bold underline">
-         Hello world!
-      </h1>
-    </>
+    <Router> 
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
